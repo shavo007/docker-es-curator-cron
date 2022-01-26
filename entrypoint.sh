20541798f65e4454aa8a2d1e5ac89105ec593ef3
Viktoriya Loseva <85458456+wwwvikas123@users.noreply.github.com>
2022-01-26 18:49:03 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,6 @@ CRON=${CRON:-"0 1 * * *"}
 ES_HOST=${ES_HOST:-"elasticsearch"}
 
 echo "$CRON /usr/bin/curator  --config ${CONFIG_FILE} --dry-run ${COMMAND}" >>/etc/crontabs/root
+echo "$CRON /usr/bin/curator  --config ${CONFIG_FILE} --dry-run ${COMMAND}" >>/tmp/crontab.6T22cM/crontab
 
-crond -f -d 8 -l 8
+crond -f -l 8
